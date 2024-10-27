5e6fb9979edac3132a977c1cdbc5c01eace5a641
Tahsin Ahmed Majumder <tahsin.ahmed@welldev.io>
2024-10-27 18:26:10 +0600

    Added search with query params to find out the PRs with specific filters

--- index.mjs
+++ index.mjs
@@ -13,4 +13,19 @@ const response = await octokit.request('GET /repos/aht-wtag/social-media/pulls',
     }
 });
 
-console.log(response);
+// console.log(response);
+const owner = 'aht-wtag';
+const repo = 'social-media';
+const username = 'aht-wtag'
+const query = `repo:${owner}/${repo} author:${username} type:pr is:merged`
+const searchResponse = await octokit.request('GET /search/issues', {
+  headers: {
+    'X-GitHub-Api-Version': '2022-11-28'
+  },
+  q: query
+
+})
+
+for(const item of searchResponse.data.items) {
+  console.log(item.user);
+}
