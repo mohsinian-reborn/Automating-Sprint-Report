2f0e82972dd6fc50c210e3bbf3f288eb08dd5485
Tahsin Ahmed Majumder <tahsin.ahmed@welldev.io>
2024-11-10 18:02:28 +0600

    fixed typo

--- db/connection.mjs
+++ db/connection.mjs
@@ -4,7 +4,7 @@ import 'dotenv/config';
 
 class Mongo {
   async connect() {
-    const user = process.env.MOGNODB_USER;
+    const user = process.env.MONGODB_USER;
     const password = process.env.MONGODB_PASSWORD;
     const url = process.env.MONGODB_URI;
     const databaseName = process.env.MONGODB_DATABASE;
